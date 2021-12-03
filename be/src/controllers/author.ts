84e2aa49fd0bec2b40649151e13c43e35cdc9b30
Arkadiy Krava <arkadiy@ex.ua>
2021-12-03 14:48:32 +0200

    controllers for authors and issuers

--- be/src/controllers/author.ts
+++ be/src/controllers/author.ts
@@ -2,7 +2,20 @@ import BookModel from "../models/book";
 
 
 class Author {
+    static async getAllAuthors() {
+        const books = await BookModel.find();
+        const authors = books.map(x => x.authors).flat();
+        const uniqueAuthors = [ ...new Set(authors) ];
+        return uniqueAuthors;
+    }
 
+    static async getAllBooksFormAuthor(author: string) {
+        const books = await BookModel.find();
+        const authorTrimmed = author.toLowerCase().trim();
+
+        const booksFiltered = books.filter(x => x.authors.some(y => y.toLowerCase().trim() === authorTrimmed));
+        return booksFiltered;
+    }
 }
 
 export default Author;
--- be/src/controllers/issuer.ts
+++ be/src/controllers/issuer.ts
@@ -2,7 +2,20 @@ import BookModel from "../models/book";
 
 
 class Issuer {
+    static async getAllIssuers() {
+        const books = await BookModel.find();
+        const issuers = books.map(x => x.issuer);
+        const uniqueIssuers = [ ...new Set(issuers) ];
+        return uniqueIssuers;
+    }
 
+    static async getAllBooksFormIssuer(issuer: string) {
+        const books = await BookModel.find();
+        const issuerTrimmed = issuer.toLowerCase().trim();
+
+        const booksFiltered = books.filter(x => x.issuer === issuerTrimmed);
+        return booksFiltered;
+    }
 }
 
 export default Issuer;
